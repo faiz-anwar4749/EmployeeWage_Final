5efb3931863221e92becbbffe978de09801f927d
Faiz <faiz.anwar4749@gmail.com>
2020-09-29 16:06:49 +0530

    [Faiz] Refactor : Committing MonthlyWage Program (UC5)

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,23 +9,27 @@ namespace EmployeeWage
             int IS_FULL_TIME = 2;
             int IS_PART_TIME = 1;
             int EMP_WAGE_PER_HOUR = 20;
-            int empHours, empWage;
+            int WorkingDays = 20;
+            int empHours, empWage = 0;
             Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            for (int i = 0; i < WorkingDays; i++)
             {
-                case 2:
-                    empHours = 8;
-                    break;
-                case 1:
-                    empHours = 4;
-                    break;
-                default:
-                    empHours = 0;
-                    break;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case 2:
+                        empHours = 8;
+                        break;
+                    case 1:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                empWage += empHours * EMP_WAGE_PER_HOUR;
             }
-            empWage = empHours * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Employee Wages: " + empWage);
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
