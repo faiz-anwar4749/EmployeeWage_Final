2d376d5a86889bfe8ae2032ee4209abc9460c955
Faiz <faiz.anwar4749@gmail.com>
2020-09-29 15:51:01 +0530

    [Faiz] Refactor : Committing PartTimeWages Program (UC3)

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,22 +6,24 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
             int EMP_WAGE_PER_HOUR = 20;
-            int empHours = 0;
-            int empWage = 0;
+            int empHours, empWage;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int check = random.Next(0, 3);
+            if (check == IS_FULL_TIME)
             {
                 empHours = 8;
             }
-            else
+            else if (check == IS_PART_TIME)
             {
-                empHours = 0;
+                empHours = 4;
             }
+            else
+                empHours = 0;
             empWage = empHours * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Employee Wage is: " + empWage);
+            Console.WriteLine("Employee Wages: " + empWage);
         }
     }
 }
