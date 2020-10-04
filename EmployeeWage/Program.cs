9dfafe392be0e829c979c93237e25f5f70f29837
Faiz <faiz.anwar4749@gmail.com>
2020-10-04 14:15:17 +0530

    [Faiz] Refactor: ComputeEmpWages for Multiple Companies

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,16 +6,13 @@ namespace EmployeeWage
     {
         public const int IS_FULL_TIME = 2;
         public const int IS_PART_TIME = 1;
-        public const int EMP_WAGE_PER_HOUR = 20;
-        public const int MaxWorkingHours = 100;
-        public const int MaxWorkingDays = 20;
-        public static int ComputeEmpWage()
+        public static int ComputeEmpWage(string company, int empWagePerHour, int maxWorkingDays, int maxWorkingHours)
         {
             int empHours, empWage = 0;
             int workingDays = 0;
             int workingHours = 0;
             Random random = new Random();
-            while (workingDays < MaxWorkingDays && workingHours < MaxWorkingHours)
+            while (workingDays < maxWorkingDays && workingHours < maxWorkingHours)
             {
                 workingDays++;
                 int check = random.Next(0, 3);
@@ -32,14 +29,15 @@ namespace EmployeeWage
                         break;
                 }
                 workingHours += empHours;
-                empWage += empHours * EMP_WAGE_PER_HOUR;
             }
-            Console.WriteLine("Employee Wage: " + empWage);
+            empWage += workingHours * empWagePerHour;
+            Console.WriteLine("Total Employee Wage for the company " + company + " is: " + empWage);
             return empWage;
         }
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("DMart", 20, 20, 100);
+            ComputeEmpWage("RJio", 20, 17, 97);
         }
     }
 }
